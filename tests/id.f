bae3782044a4f93d73a014cb7b68025f758b1853
Meleagant <josselin.giet@ens.fr>
2020-05-30 00:16:14 +1000

    moving tests in separate directory

---
+++ tests/id.f
@@ -0,0 +1,3 @@
+-- this is the basic polymorphic identity function in system F.
+
+\l α : *. \l x : α . x
---
+++ tests/lx_xx.f
@@ -0,0 +1,3 @@
+-- This example shows that we can type the term \lambx.xx in system F.
+-- it is the identity function of the identity function
+\lamb (x: (\P (α : *). \P (s: α). α)). x (\P (α: *).\P (_ : α).  α) x
